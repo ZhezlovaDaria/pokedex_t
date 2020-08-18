9fb8bfdf081cf980bf9d7a10ba05bd12369d58c2
Zhezlova Daria <zh.daria4@gmail.com>
2020-08-18 14:53:41 +0300

    pokemon card p1

--- app/src/main/java/com/example/pocedex/PokeCard.java
+++ app/src/main/java/com/example/pocedex/PokeCard.java
@@ -2,22 +2,27 @@ package com.example.pocedex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import org.json.JSONArray;
-import org.json.JSONException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.InputStream;
+import java.net.URL;
 
 public class PokeCard extends AppCompatActivity {
 
     private static String pok = "";
     JSONParser jsonParser = new JSONParser();
-
+    Pokemon pokemon;
+    Bitmap bitmap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,8 +32,6 @@ public class PokeCard extends AppCompatActivity {
 
         new Card().execute();
     }
-
-
     class Card extends AsyncTask<String, String, String> {
         @Override
         protected void onPreExecute() {
@@ -37,30 +40,31 @@ public class PokeCard extends AppCompatActivity {
 
         protected String doInBackground(String... args) {
             JSONObject json = jsonParser.makeHttpRequest(pok);
+            String s=json.toString();
+            GsonBuilder builder = new GsonBuilder();
+            Gson gson = builder.create();
+            pokemon = gson.fromJson(s, Pokemon.class);
+            try {
 
+                InputStream is = new URL(pokemon.sprites.other.official_artwork.front_default).openStream();
+                bitmap = BitmapFactory.decodeStream(is);
+            }
+            catch (Exception e)
+            {
+                Log.d("Fail Image", pokemon.sprites.other.official_artwork.front_default);
+            }
             Log.d("Create Response", json.toString());
 
             return null;
         }
 
         protected void onPostExecute(String file_url) {
+            TextView tv1 = (TextView) findViewById(R.id.pokname);
+            TextView tv2 = (TextView) findViewById(R.id.ab);
+            ImageView iv1 =(ImageView) findViewById(R.id.pokeim);
+            tv1.setText(pokemon.getName());
+            iv1.setImageBitmap(bitmap);
         }
 
-
-        public  Pokemon decodeJSON(JSONObject json)
-        {
-            try {
-                String name = json.get("name").toString();
-                String link = json.get("url").toString();
-                Pokemon p=new Pokemon();
-                p.setName(name);
-                p.setLink(link);
-                return p;
-
-            }catch (Exception e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
     }
 }
--- app/src/main/java/com/example/pocedex/Pokemon.java
+++ app/src/main/java/com/example/pocedex/Pokemon.java
@@ -1,12 +1,104 @@
 package com.example.pocedex;
 
+
+import com.google.gson.annotations.SerializedName;
+
 public class Pokemon {
 
+    public int id;
+    public boolean is_default;
     private String name;
     private String link;
+    public ability[] abilities;
+    public form[] forms;
+    public game_indice[] game_indices;
+    public String height;
+    public String[] held_item;
+    public move[] moves;
+    public sprite sprites;
+    public stat[] stats;
+    public type[] types;
+    public String weight;
+    public String order;
+    public String location_area_encounters;
 
     public void setName(String name){this.name=name;}
     public String getName() {return name;}
     public void setLink(String link){this.link=link;}
     public String getLink() {return link;}
+
+
+    public class ability
+    {
+        public Ab ability;
+        public boolean is_hidden;
+        public int slot;
+        public class Ab
+        {
+            public String name;
+            public String url;
+        }
+    }
+
+    public class form
+    {
+        public String name;
+        public String url;
+    }
+    public class game_indice
+    {
+        public int game_index;
+        public Version version;
+        public class Version {
+            public String name;
+            public String url;
+        }
+    }
+    public class move
+    {
+        public String name;
+        public String url;
+    }
+    public class sprite
+    {
+        public String back_default;
+        public String back_female;
+        public String back_shiny;
+        public String back_shiny_female;
+        public String front_default;
+        public String front_female;
+        public String front_shiny;
+        public String front_shiny_female;
+        public Other other;
+
+        public class Other
+        {
+            public dw dream_world;
+            @SerializedName("official-artwork")
+            public ow official_artwork;
+            public class dw
+            {
+                public String front_default;
+                public String front_female;
+            }
+            public class ow
+            {
+                public String front_default;
+            }
+        }
+    }
+    public class stat
+    {
+        public int base_stat;
+        public int effot;
+        public String name;
+        public String url;
+    }
+    public class type
+    {
+        public int slot;
+        public String name;
+        public String url;
+    }
 }
+
